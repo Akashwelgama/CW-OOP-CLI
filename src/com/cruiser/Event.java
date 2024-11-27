2cbb5e3289d0321973a9c3777d248857eb0d143b
Akash Welgama <139337308+Akashwelgama@users.noreply.github.com>
2024-11-27 08:35:45 +0530

    Commit 3
    Background setting: Done
    Just a matter of setting the initiation on main

--- src/com/cruiser/Event.java
+++ src/com/cruiser/Event.java
@@ -38,64 +38,86 @@ public class Event implements Runnable {
 
     }
 
-    //time to implement the customer manager function
-
-
-    public void engine(){
-
-    }
-
-
 
 
 }
+
+
+
+
 
 
 class VendorEngine implements Runnable{
 
     private Event event;
 
-    public void VendorManager(){
-        List<Vendor> customerSet =
+
+    public void vendorManager(){
+        List<Vendor> vendorSet =
                 Person.createPeople(Vendor.class, event.getEventDynamics().getTicketRetrievalRate(), event);
+        while(!event.isTerminate()){
+            for(Vendor vendor: vendorSet){
+                if (!(event.getTable().isVendorWaitingRoomFull())) continue;
+                Thread customerThread = new Thread(vendor);
+                customerThread.start();
+            }
+
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println("Problem occurred when initiating the vendor threads");
+            }
+        }
     }
     @Override
     public void run() {
-
+        vendorManager();
     }
-    public VendorEngine(Event event){
+
+    VendorEngine(Event event) {
         this.event = event;
     }
 
 }
 
+
+
+
+
+
 class CustomerEngine implements Runnable{
 
     private Event event;
+
+
 
     public void customerManager(){
         List<Customer> customerSet =
                 Person.createPeople(Customer.class, event.getEventDynamics().getTicketRetrievalRate(), event);
 
         while(!event.isTerminate()){
+            for(Customer customer: customerSet){
+                if (!(event.getTable().isCustomerWaitingRoomFull())) continue;
+                Thread customerThread = new Thread(customer);
+                customerThread.start();
+            }
 
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println("Problem occurred when initiating the customer threads");
+            }
         }
-    }
-
-    public static void starter(Event event){
-
     }
 
 
     @Override
     public void run() {
-
+        customerManager();
     }
 
-    public CustomerEngine(Event event){
+    CustomerEngine(Event event) {
         this.event = event;
     }
-
-
 
 }
